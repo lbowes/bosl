87abc9002cbdeaf6dc1b9ded80ad4b981f73db43
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2023-01-07 02:54:32 +0000

    Add new Work, Proficiency classes to data

--- data.py
+++ data.py
@@ -1,13 +1,64 @@
-# TODO: This file should define a language with which all real organisations can
-# be described.
+# TODO: This file should define a language that makes it possible to encode the
+# state of any real organisation, defined as a group of individuals working
+# towards a shared goal.
+# What is true of *any* organisation?
+
+class Person:
+    pass
 
 
-class Organisation():
+class Organisation:
     def __init__(self):
-        self._mission_statement = ""
-        self._members = list()
+        self._members: set[Person] = set()
+
+    def enrol(self, new_member: Person) -> None:
+        self._members.add(new_member)
 
 
-class Member():
-    def __init__(self, name):
-        self._name = name
+class Work:
+    def __init__(self):
+        # todo: what relationship does this have to expertise?
+        # value(expertise, work) ?
+        pass
+        #self._requirements: list[]
+
+
+class Proficiency(float):
+    """A measure of expertise in either knowledge or skill."""
+
+    MIN = 0.0
+    MAX = 1.0 # The most proficient human on Earth for a given understanding/skill
+
+    def __new__(cls, value: float):
+        if not cls.MIN <= value <= cls.MAX:
+            raise ValueError(f"Proficiency must be between {cls.MIN} and {cls.MAX}")
+
+        return super().__new__(cls, value)
+
+
+Expertise = list[Proficiency]
+
+
+class Person:
+    def __init__(self, name: str, expertise: Expertise) -> None:
+        self._name: str = name
+        self._expertise: Expertise = [Proficiency(x) for x in expertise]
+
+    def join(self, org: Organisation) -> None:
+        org.enrol(self)
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    @property
+    def expertise(self) -> str:
+        return self._expertise
+
+    @expertise.setter
+    def expertise(self, value: str):
+        self._expertise = value
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from data import Organisation
+from data import Organisation, Person
 
 
 def main() -> None:
     org = Organisation()
+
+    alice = Person("alice", expertise=[0.1, 0.0, 0.9])
+    bob = Person("bob", expertise=[0.5, 0.3, 0.4])
+
+    alice.join(org)
+
+    for m in org._members:
+        print(m.name)
 
 
 if __name__ == '__main__':
