832a8298ce41b816ef1cd8e4c9a8bff81f2298f5
Lewis Bowes <33807016+lbowes@users.noreply.github.com>
2023-01-17 13:12:34 +0000

    Add list of activities back into organisation

--- model/organisation.py
+++ model/organisation.py
@@ -1,11 +1,13 @@
 # What is true of any organisation?
 
 from model.person import Person
+from model.activity import Activity
 
 
 class Organisation:
     def __init__(self):
         self._members: set[Person] = set()
+        self._activities: set[Activity] = set()
 
     def enrol(self, new_member: Person) -> None:
         self._members.add(new_member)
